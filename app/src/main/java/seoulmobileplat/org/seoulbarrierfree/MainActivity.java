b77931980cfe568ba22d1454149588d249e3100a
gyeomo <canino@naver.com>
2019-04-17 16:48:48 +0900

    initial commit

--- app/src/main/java/seoulmobileplat/org/seoulbarrierfree/MainActivity.java
+++ app/src/main/java/seoulmobileplat/org/seoulbarrierfree/MainActivity.java
@@ -8,14 +8,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -578,43 +576,17 @@ public class MainActivity extends AppCompatActivity{
 
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
-            //여기서 위치값이 갱신되면 이벤트가 발생한다.
-            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
-            Geocoder mGeoCoder = new Geocoder(getBaseContext());
-
-
-            Log.d("test", "onLocationChanged, location:" + location);
             longitude = location.getLongitude(); //경도
             latitude = location.getLatitude();   //위도
-            double altitude = location.getAltitude();   //고도
-            float accuracy = location.getAccuracy();    //정확도
-            String provider = location.getProvider();   //위치제공자
-            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
-            //Network 위치제공자에 의한 위치변화
-            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
-                //try{ //http://developer88.tistory.com/135
-                   //List<Address> mResultList = mGeoCoder.getFromLocation(latitude,longitude,1);
-                   //tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
-                  //         + "\n고도 : " + altitude + "\n정확도 : "  + accuracy+"\n주소 : "+mResultList.get(0).getAddressLine(0));
-               //}catch (IOException e){
-               //   e.printStackTrace();
-              //}
-
         }
         public void onProviderDisabled(String provider) {
-            // Disabled시
-            Log.d("test", "onProviderDisabled, provider:" + provider);
         }
 
         public void onProviderEnabled(String provider) {
-            // Enabled시
-            Log.d("test", "onProviderEnabled, provider:" + provider);
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
-            // 변경시
-            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
-        }
+       }
     };
     // GPS Disabled Alert
     private void createGpsDisabledAlert() {
